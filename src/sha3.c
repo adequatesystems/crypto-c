4df0064a7a823b8012ae203481ba3e8914ea0bd6
Chrisdigity <chrisdigity@gmail.com>
2022-01-21 08:20:02 +1000

    implement SHA3 optimisations and doxygen compatibility

--- src/sha3.c
+++ src/sha3.c
@@ -1,51 +1,22 @@
 /**
- * sha3.c - SHA3 and Keccak hash function support
- *
- * Based on Dr. Markku-Juhani O. Saarinen's (mjos@iki.fi)
- * "cooked up" compact and readable keccak implemetation,
- *    <https://github.com/mjosaarinen/tiny_sha3>
- * which was released under the MIT license (MIT) and is therefore
- * used with permission, and with much gratitude.  \(^-^)/
- *
- * Original works Copyright (c) 2015 Markku-Juhani O. Saarinen
- * Modified works Copyright (c) 2020-2021 Adequate Systems, LLC.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * For more information, please refer to ../LICENSE.MIT
- *
- * Date: 22 April 2020
- * Revised: 26 October 2021
- *
- * NOTES:
- * - Presently, due to heavy reliance on 64-bit operations, this
- *   implementation CANNOT RUN on systems that rely on C89 compliance.
- * - This implementation supports the SHA-3 and Keccak algorithm
- *   variants for 224, 256, 384 and 512 bit message digests on x86
- *   and x64 little endian systems, using modified routines and
- *   unrolled loops for faster Keccak permutations.
- * - This implementation relies on custom datatypes declared within
- *   a custom library. However, in the absense of such a library,
- *   functionality may be reinstated by simply redeclaring
- *   datatypes as appropriate for the target system.
- *
+ * @private
+ * @headerfile sha3.h <sha3.h>
+ * @copyright This file is released under The MIT License (MIT).
+ * For more information, see the header file...
 */
 
-#ifndef _CRYPTO_SHA3_C_
-#define _CRYPTO_SHA3_C_  /* include guard */
+/* include guard */
+#ifndef CRYPTO_SHA3_C
+#define CRYPTO_SHA3_C
 
 
 #include "sha3.h"
+#include <string.h>
 
-static const word64 keccakf_rndc[24] = {
+#define KECCAKFROUNDS   24
+
+/* Keccakf round constant */
+ALIGN(32) static const uint64_t keccakf_rndc[24] = {
    0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
    0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
    0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
@@ -57,169 +28,316 @@ static const word64 keccakf_rndc[24] = {
 };
 
 /* SHA3_Keccak permutation */
-static void sha3_keccakf(word64 st[])
+HOST_DEVICE_FN static void sha3_keccakf(uint64_t st[])
 {
-   word64 t, bc[5];
-   int r;
+	ALIGN(8) uint64_t Ba, Be, Bi, Bo, Bu;
+	ALIGN(8) uint64_t Ca, Ce, Ci, Co, Cu;
+	ALIGN(8) uint64_t Da, De, Di, Do, Du;
+	int r;
 
-   for (r = 0; r < KECCAKFROUNDS; r++) {
-      bc[0] = st[0] ^ st[5] ^ st[10] ^ st[15] ^ st[20];
-      bc[1] = st[1] ^ st[6] ^ st[11] ^ st[16] ^ st[21];
-      bc[2] = st[2] ^ st[7] ^ st[12] ^ st[17] ^ st[22];
-      bc[3] = st[3] ^ st[8] ^ st[13] ^ st[18] ^ st[23];
-      bc[4] = st[4] ^ st[9] ^ st[14] ^ st[19] ^ st[24];
-      t = bc[4] ^ ROTL64(bc[1], 1);
-      st[0] ^= t;
-      st[5] ^= t;
-      st[10] ^= t;
-      st[15] ^= t;
-      st[20] ^= t;
-      t = bc[0] ^ ROTL64(bc[2], 1);
-      st[1] ^= t;
-      st[6] ^= t;
-      st[11] ^= t;
-      st[16] ^= t;
-      st[21] ^= t;
-      t = bc[1] ^ ROTL64(bc[3], 1);
-      st[2] ^= t;
-      st[7] ^= t;
-      st[12] ^= t;
-      st[17] ^= t;
-      st[22] ^= t;
-      t = bc[2] ^ ROTL64(bc[4], 1);
-      st[3] ^= t;
-      st[8] ^= t;
-      st[13] ^= t;
-      st[18] ^= t;
-      st[23] ^= t;
-      t = bc[3] ^ ROTL64(bc[0], 1);
-      st[4] ^= t;
-      st[9] ^= t;
-      st[14] ^= t;
-      st[19] ^= t;
-      st[24] ^= t;
-      t = st[1];
-      st[1]  = ROTL64(st[6],  44);
-      st[6]  = ROTL64(st[9],  20);
-      st[9]  = ROTL64(st[22], 61);
-      st[22] = ROTL64(st[14], 39);
-      st[14] = ROTL64(st[20], 18);
-      st[20] = ROTL64(st[2],  62);
-      st[2]  = ROTL64(st[12], 43);
-      st[12] = ROTL64(st[13], 25);
-      st[13] = ROTL64(st[19], 8);
-      st[19] = ROTL64(st[23], 56);
-      st[23] = ROTL64(st[15], 41);
-      st[15] = ROTL64(st[4],  27);
-      st[4]  = ROTL64(st[24], 14);
-      st[24] = ROTL64(st[21], 2);
-      st[21] = ROTL64(st[8],  55);
-      st[8]  = ROTL64(st[16], 45);
-      st[16] = ROTL64(st[5],  36);
-      st[5]  = ROTL64(st[3],  28);
-      st[3]  = ROTL64(st[18], 21);
-      st[18] = ROTL64(st[17], 15);
-      st[17] = ROTL64(st[11], 10);
-      st[11] = ROTL64(st[7],  6);
-      st[7]  = ROTL64(st[10], 3);
-      st[10] = ROTL64(t, 1);
-      bc[0] = st[0];
-      bc[1] = st[1];
-      st[0] ^= (~st[1]) & st[2];
-      st[1] ^= (~st[2]) & st[3];
-      st[2] ^= (~st[3]) & st[4];
-      st[3] ^= (~st[4]) & bc[0];
-      st[4] ^= (~bc[0]) & bc[1];
-      bc[0] = st[5];
-      bc[1] = st[6];
-      st[5] ^= (~st[6]) & st[7];
-      st[6] ^= (~st[7]) & st[8];
-      st[7] ^= (~st[8]) & st[9];
-      st[8] ^= (~st[9]) & bc[0];
-      st[9] ^= (~bc[0]) & bc[1];
-      bc[0] = st[10];
-      bc[1] = st[11];
-      st[10] ^= (~st[11]) & st[12];
-      st[11] ^= (~st[12]) & st[13];
-      st[12] ^= (~st[13]) & st[14];
-      st[13] ^= (~st[14]) & bc[0];
-      st[14] ^= (~bc[0]) & bc[1];
-      bc[0] = st[15];
-      bc[1] = st[16];
-      st[15] ^= (~st[16]) & st[17];
-      st[16] ^= (~st[17]) & st[18];
-      st[17] ^= (~st[18]) & st[19];
-      st[18] ^= (~st[19]) & bc[0];
-      st[19] ^= (~bc[0]) & bc[1];
-      bc[0] = st[20];
-      bc[1] = st[21];
-      st[20] ^= (~st[21]) & st[22];
-      st[21] ^= (~st[22]) & st[23];
-      st[22] ^= (~st[23]) & st[24];
-      st[23] ^= (~st[24]) & bc[0];
-      st[24] ^= (~bc[0]) & bc[1];
-      st[0] ^= keccakf_rndc[r];
-   }
-}
+	for (r = 0; r < KECCAKFROUNDS; r += 4) {
+		/* Unrolled 4 rounds at a time */
+
+		Ca = st[0] ^ st[5] ^ st[10] ^ st[15] ^ st[20];
+		Ce = st[1] ^ st[6] ^ st[11] ^ st[16] ^ st[21];
+		Ci = st[2] ^ st[7] ^ st[12] ^ st[17] ^ st[22];
+		Co = st[3] ^ st[8] ^ st[13] ^ st[18] ^ st[23];
+		Cu = st[4] ^ st[9] ^ st[14] ^ st[19] ^ st[24];
+		Da = Cu ^ rol64(Ce, 1);
+		De = Ca ^ rol64(Ci, 1);
+		Di = Ce ^ rol64(Co, 1);
+		Do = Ci ^ rol64(Cu, 1);
+		Du = Co ^ rol64(Ca, 1);
+
+		Ba = (st[0] ^ Da);
+		Be = rol64((st[6] ^ De), 44);
+		Bi = rol64((st[12] ^ Di), 43);
+		Bo = rol64((st[18] ^ Do), 21);
+		Bu = rol64((st[24] ^ Du), 14);
+		st[0]  = Ba ^ ((~Be) & Bi) ^ keccakf_rndc[r];
+		st[6]  = Be ^ ((~Bi) & Bo);
+		st[12] = Bi ^ ((~Bo) & Bu);
+		st[18] = Bo ^ ((~Bu) & Ba);
+		st[24] = Bu ^ ((~Ba) & Be);
+
+		Bi = rol64((st[10] ^ Da), 3);
+		Bo = rol64((st[16] ^ De), 45);
+		Bu = rol64((st[22] ^ Di), 61);
+		Ba = rol64((st[3] ^ Do), 28);
+		Be = rol64((st[9] ^ Du), 20);
+		st[10] = Ba ^ ((~Be) & Bi);
+		st[16] = Be ^ ((~Bi) & Bo);
+		st[22] = Bi ^ ((~Bo) & Bu);
+		st[3] = Bo ^ ((~Bu) & Ba);
+		st[9] = Bu ^ ((~Ba) & Be);
+
+		Bu = rol64((st[20] ^ Da), 18);
+		Ba = rol64((st[1] ^ De), 1);
+		Be = rol64((st[7] ^ Di), 6);
+		Bi = rol64((st[13] ^ Do), 25);
+		Bo = rol64((st[19] ^ Du), 8);
+		st[20] = Ba ^ ((~Be) & Bi);
+		st[1] = Be ^ ((~Bi) & Bo);
+		st[7] = Bi ^ ((~Bo) & Bu);
+		st[13] = Bo ^ ((~Bu) & Ba);
+		st[19] = Bu ^ ((~Ba) & Be);
+
+		Be = rol64((st[5] ^ Da), 36);
+		Bi = rol64((st[11] ^ De), 10);
+		Bo = rol64((st[17] ^ Di), 15);
+		Bu = rol64((st[23] ^ Do), 56);
+		Ba = rol64((st[4] ^ Du), 27);
+		st[5] = Ba ^ ((~Be) & Bi);
+		st[11] = Be ^ ((~Bi) & Bo);
+		st[17] = Bi ^ ((~Bo) & Bu);
+		st[23] = Bo ^ ((~Bu) & Ba);
+		st[4] = Bu ^ ((~Ba) & Be);
+
+		Bo = rol64((st[15] ^ Da), 41);
+		Bu = rol64((st[21] ^ De), 2);
+		Ba = rol64((st[2] ^ Di), 62);
+		Be = rol64((st[8] ^ Do), 55);
+		Bi = rol64((st[14] ^ Du), 39);
+		st[15] = Ba ^ ((~Be) & Bi);
+		st[21] = Be ^ ((~Bi) & Bo);
+		st[2] = Bi ^ ((~Bo) & Bu);
+		st[8] = Bo ^ ((~Bu) & Ba);
+		st[14] = Bu ^ ((~Ba) & Be);
+
+		Ca = st[0] ^ st[10] ^ st[20] ^ st[5] ^ st[15];
+		Ce = st[6] ^ st[16] ^ st[1] ^ st[11] ^ st[21];
+		Ci = st[12] ^ st[22] ^ st[7] ^ st[17] ^ st[2];
+		Co = st[18] ^ st[3] ^ st[13] ^ st[23] ^ st[8];
+		Cu = st[24] ^ st[9] ^ st[19] ^ st[4] ^ st[14];
+		Da = Cu ^ rol64(Ce, 1);
+		De = Ca ^ rol64(Ci, 1);
+		Di = Ce ^ rol64(Co, 1);
+		Do = Ci ^ rol64(Cu, 1);
+		Du = Co ^ rol64(Ca, 1);
+
+		Ba = (st[0] ^ Da);
+		Be = rol64((st[16] ^ De), 44);
+		Bi = rol64((st[7] ^ Di), 43);
+		Bo = rol64((st[23] ^ Do), 21);
+		Bu = rol64((st[14] ^ Du), 14);
+		st[0] = Ba ^ ((~Be) & Bi) ^ keccakf_rndc[r + 1];
+		st[16] = Be ^ ((~Bi) & Bo);
+		st[7] = Bi ^ ((~Bo) & Bu);
+		st[23] = Bo ^ ((~Bu) & Ba);
+		st[14] = Bu ^ ((~Ba) & Be);
+
+		Bi = rol64((st[20] ^ Da), 3);
+		Bo = rol64((st[11] ^ De), 45);
+		Bu = rol64((st[2] ^ Di), 61);
+		Ba = rol64((st[18] ^ Do), 28);
+		Be = rol64((st[9] ^ Du), 20);
+		st[20] = Ba ^ ((~Be) & Bi);
+		st[11] = Be ^ ((~Bi) & Bo);
+		st[2] = Bi ^ ((~Bo) & Bu);
+		st[18] = Bo ^ ((~Bu) & Ba);
+		st[9] = Bu ^ ((~Ba) & Be);
+
+		Bu = rol64((st[15] ^ Da), 18);
+		Ba = rol64((st[6] ^ De), 1);
+		Be = rol64((st[22] ^ Di), 6);
+		Bi = rol64((st[13] ^ Do), 25);
+		Bo = rol64((st[4] ^ Du), 8);
+		st[15] = Ba ^ ((~Be) & Bi);
+		st[6] = Be ^ ((~Bi) & Bo);
+		st[22] = Bi ^ ((~Bo) & Bu);
+		st[13] = Bo ^ ((~Bu) & Ba);
+		st[4] = Bu ^ ((~Ba) & Be);
+
+		Be = rol64((st[10] ^ Da), 36);
+		Bi = rol64((st[1] ^ De), 10);
+		Bo = rol64((st[17] ^ Di), 15);
+		Bu = rol64((st[8] ^ Do), 56);
+		Ba = rol64((st[24] ^ Du), 27);
+		st[10] = Ba ^ ((~Be) & Bi);
+		st[1] = Be ^ ((~Bi) & Bo);
+		st[17] = Bi ^ ((~Bo) & Bu);
+		st[8] = Bo ^ ((~Bu) & Ba);
+		st[24] = Bu ^ ((~Ba) & Be);
+
+		Bo = rol64((st[5] ^ Da), 41);
+		Bu = rol64((st[21] ^ De), 2);
+		Ba = rol64((st[12] ^ Di), 62);
+		Be = rol64((st[3] ^ Do), 55);
+		Bi = rol64((st[19] ^ Du), 39);
+		st[5] = Ba ^ ((~Be) & Bi);
+		st[21] = Be ^ ((~Bi) & Bo);
+		st[12] = Bi ^ ((~Bo) & Bu);
+		st[3] = Bo ^ ((~Bu) & Ba);
+		st[19] = Bu ^ ((~Ba) & Be);
+
+		Ca = st[0] ^ st[20] ^ st[15] ^ st[10] ^ st[5];
+		Ce = st[16] ^ st[11] ^ st[6] ^ st[1] ^ st[21];
+		Ci = st[7] ^ st[2] ^ st[22] ^ st[17] ^ st[12];
+		Co = st[23] ^ st[18] ^ st[13] ^ st[8] ^ st[3];
+		Cu = st[14] ^ st[9] ^ st[4] ^ st[24] ^ st[19];
+		Da = Cu ^ rol64(Ce, 1);
+		De = Ca ^ rol64(Ci, 1);
+		Di = Ce ^ rol64(Co, 1);
+		Do = Ci ^ rol64(Cu, 1);
+		Du = Co ^ rol64(Ca, 1);
+
+		Ba = (st[0] ^ Da);
+		Be = rol64((st[11] ^ De), 44);
+		Bi = rol64((st[22] ^ Di), 43);
+		Bo = rol64((st[8] ^ Do), 21);
+		Bu = rol64((st[19] ^ Du), 14);
+		st[0] = Ba ^ ((~Be) & Bi) ^ keccakf_rndc[r + 2];
+		st[11] = Be ^ ((~Bi) & Bo);
+		st[22] = Bi ^ ((~Bo) & Bu);
+		st[8] = Bo ^ ((~Bu) & Ba);
+		st[19] = Bu ^ ((~Ba) & Be);
+
+		Bi = rol64((st[15] ^ Da), 3);
+		Bo = rol64((st[1] ^ De), 45);
+		Bu = rol64((st[12] ^ Di), 61);
+		Ba = rol64((st[23] ^ Do), 28);
+		Be = rol64((st[9] ^ Du), 20);
+		st[15] = Ba ^ ((~Be) & Bi);
+		st[1] = Be ^ ((~Bi) & Bo);
+		st[12] = Bi ^ ((~Bo) & Bu);
+		st[23] = Bo ^ ((~Bu) & Ba);
+		st[9] = Bu ^ ((~Ba) & Be);
+
+		Bu = rol64((st[5] ^ Da), 18);
+		Ba = rol64((st[16] ^ De), 1);
+		Be = rol64((st[2] ^ Di), 6);
+		Bi = rol64((st[13] ^ Do), 25);
+		Bo = rol64((st[24] ^ Du), 8);
+		st[5] = Ba ^ ((~Be) & Bi);
+		st[16] = Be ^ ((~Bi) & Bo);
+		st[2] = Bi ^ ((~Bo) & Bu);
+		st[13] = Bo ^ ((~Bu) & Ba);
+		st[24] = Bu ^ ((~Ba) & Be);
+
+		Be = rol64((st[20] ^ Da), 36);
+		Bi = rol64((st[6] ^ De), 10);
+		Bo = rol64((st[17] ^ Di), 15);
+		Bu = rol64((st[3] ^ Do), 56);
+		Ba = rol64((st[14] ^ Du), 27);
+		st[20] = Ba ^ ((~Be) & Bi);
+		st[6] = Be ^ ((~Bi) & Bo);
+		st[17] = Bi ^ ((~Bo) & Bu);
+		st[3] = Bo ^ ((~Bu) & Ba);
+		st[14] = Bu ^ ((~Ba) & Be);
+
+		Bo = rol64((st[10] ^ Da), 41);
+		Bu = rol64((st[21] ^ De), 2);
+		Ba = rol64((st[7] ^ Di), 62);
+		Be = rol64((st[18] ^ Do), 55);
+		Bi = rol64((st[4] ^ Du), 39);
+		st[10] = Ba ^ ((~Be) & Bi);
+		st[21] = Be ^ ((~Bi) & Bo);
+		st[7] = Bi ^ ((~Bo) & Bu);
+		st[18] = Bo ^ ((~Bu) & Ba);
+		st[4] = Bu ^ ((~Ba) & Be);
+
+		Ca = st[0] ^ st[15] ^ st[5] ^ st[20] ^ st[10];
+		Ce = st[11] ^ st[1] ^ st[16] ^ st[6] ^ st[21];
+		Ci = st[22] ^ st[12] ^ st[2] ^ st[17] ^ st[7];
+		Co = st[8] ^ st[23] ^ st[13] ^ st[3] ^ st[18];
+		Cu = st[19] ^ st[9] ^ st[24] ^ st[14] ^ st[4];
+		Da = Cu ^ rol64(Ce, 1);
+		De = Ca ^ rol64(Ci, 1);
+		Di = Ce ^ rol64(Co, 1);
+		Do = Ci ^ rol64(Cu, 1);
+		Du = Co ^ rol64(Ca, 1);
+
+		Ba = (st[0] ^ Da);
+		Be = rol64((st[1] ^ De), 44);
+		Bi = rol64((st[2] ^ Di), 43);
+		Bo = rol64((st[3] ^ Do), 21);
+		Bu = rol64((st[4] ^ Du), 14);
+		st[0] = Ba ^ ((~Be) & Bi) ^ keccakf_rndc[r + 3];
+		st[1] = Be ^ ((~Bi) & Bo);
+		st[2] = Bi ^ ((~Bo) & Bu);
+		st[3] = Bo ^ ((~Bu) & Ba);
+		st[4] = Bu ^ ((~Ba) & Be);
+
+		Bi = rol64((st[5] ^ Da), 3);
+		Bo = rol64((st[6] ^ De), 45);
+		Bu = rol64((st[7] ^ Di), 61);
+		Ba = rol64((st[8] ^ Do), 28);
+		Be = rol64((st[9] ^ Du), 20);
+		st[5] = Ba ^ ((~Be) & Bi);
+		st[6] = Be ^ ((~Bi) & Bo);
+		st[7] = Bi ^ ((~Bo) & Bu);
+		st[8] = Bo ^ ((~Bu) & Ba);
+		st[9] = Bu ^ ((~Ba) & Be);
+
+		Bu = rol64((st[10] ^ Da), 18);
+		Ba = rol64((st[11] ^ De), 1);
+		Be = rol64((st[12] ^ Di), 6);
+		Bi = rol64((st[13] ^ Do), 25);
+		Bo = rol64((st[14] ^ Du), 8);
+		st[10] = Ba ^ ((~Be) & Bi);
+		st[11] = Be ^ ((~Bi) & Bo);
+		st[12] = Bi ^ ((~Bo) & Bu);
+		st[13] = Bo ^ ((~Bu) & Ba);
+		st[14] = Bu ^ ((~Ba) & Be);
+
+		Be = rol64((st[15] ^ Da), 36);
+		Bi = rol64((st[16] ^ De), 10);
+		Bo = rol64((st[17] ^ Di), 15);
+		Bu = rol64((st[18] ^ Do), 56);
+		Ba = rol64((st[19] ^ Du), 27);
+		st[15] = Ba ^ ((~Be) & Bi);
+		st[16] = Be ^ ((~Bi) & Bo);
+		st[17] = Bi ^ ((~Bo) & Bu);
+		st[18] = Bo ^ ((~Bu) & Ba);
+		st[19] = Bu ^ ((~Ba) & Be);
+
+		Bo = rol64((st[20] ^ Da), 41);
+		Bu = rol64((st[21] ^ De), 2);
+		Ba = rol64((st[22] ^ Di), 62);
+		Be = rol64((st[23] ^ Do), 55);
+		Bi = rol64((st[24] ^ Du), 39);
+		st[20] = Ba ^ ((~Be) & Bi);
+		st[21] = Be ^ ((~Bi) & Bo);
+		st[22] = Bi ^ ((~Bo) & Bu);
+		st[23] = Bo ^ ((~Bu) & Ba);
+		st[24] = Bu ^ ((~Ba) & Be);
+	}
+}  /* end sha3_keccakf() */
 
 /* Initialize the hashing context `ctx` */
-void sha3_init(SHA3_CTX *ctx, int outlen)
+HOST_DEVICE_FN void sha3_init(SHA3_CTX *ctx, int outlen)
 {
-   int i;
-
-   for (i = 0; i < 25; i++) {
-      ctx->st.q[i] = 0;
-   }
-   ctx->outlen = outlen;
+   memset(ctx->st.b, 0, 200);
+   ctx->outlen = (uint32_t) outlen;
    ctx->rsiz = 200 - (ctx->outlen << 1);
    ctx->pt = 0;
 }
 
 /* Add `inlen` bytes from `in` into the hash */
-void sha3_update(SHA3_CTX *ctx, const void *in, size_t inlen)
+HOST_DEVICE_FN void sha3_update(SHA3_CTX *ctx, const void *in, size_t inlen)
 {
    size_t i;
-   int j;
 
-   j = ctx->pt;
    for (i = 0; i < inlen; i++) {
-      ctx->st.b[j++] ^= ((const word8 *) in)[i];
-      if (j >= ctx->rsiz) {
+      ctx->st.b[ctx->pt++] ^= ((const uint8_t *) in)[i];
+      if (ctx->pt == ctx->rsiz) {
          sha3_keccakf(ctx->st.q);
-         j = 0;
+         ctx->pt = 0;
       }
    }
-   ctx->pt = j;
 }
 
 /* Generate the message digest and place in `out` */
-void sha3_final(SHA3_CTX *ctx, void *out)
+HOST_DEVICE_FN void sha3_final(SHA3_CTX *ctx, void *out)
 {
    ctx->st.b[ctx->pt] ^= 0x06;
    ctx->st.b[ctx->rsiz - 1] ^= 0x80;
    sha3_keccakf(ctx->st.q);
 
-   /* 224-bit digest */
-   ((word64 *) out)[0] = ctx->st.q[0];
-   ((word64 *) out)[1] = ctx->st.q[1];
-   ((word64 *) out)[2] = ctx->st.q[2];
-   ((word32 *) out)[6] = ctx->st.d[6];
-   if (ctx->outlen <= 28) return;
-
-   /* 256-bit digest */
-   ((word32 *) out)[7] = ctx->st.d[7];
-   if (ctx->outlen <= 32) return;
-
-   /* 384-bit digest */
-   ((word64 *) out)[4] = ctx->st.q[4];
-   ((word64 *) out)[5] = ctx->st.q[5];
-   if (ctx->outlen <= 48) return;
-
-   /* 512-bit digest */
-   ((word64 *) out)[6] = ctx->st.q[6];
-   ((word64 *) out)[7] = ctx->st.q[7];
+   /* copy digest to out */
+   memcpy(out, ctx->st.q, ctx->outlen);
 }
-void keccak_final(SHA3_CTX *ctx, void *out)
+HOST_DEVICE_FN void keccak_final(SHA3_CTX *ctx, void *out)
 {
    /* This next step essentially converts the sha3_final() step
     * `c->st.b[c->pt] ^= 0x06;`  (to)  `c->st.b[c->pt] ^= 0x01;`
@@ -229,7 +347,7 @@ void keccak_final(SHA3_CTX *ctx, void *out)
 }
 
 /* Convenient all-in-one SHA3 computation */
-void sha3(const void *in, size_t inlen, void *out, int outlen)
+HOST_DEVICE_FN void sha3(const void *in, size_t inlen, void *out, int outlen)
 {
    SHA3_CTX sha3;
 
@@ -239,7 +357,7 @@ void sha3(const void *in, size_t inlen, void *out, int outlen)
 }
 
 /* Convenient all-in-one Keccak computation */
-void keccak(const void *in, size_t inlen, void *out, int outlen)
+HOST_DEVICE_FN void keccak(const void *in, size_t inlen, void *out, int outlen)
 {
    KECCAK_CTX keccak;
 
--- src/sha3.h
+++ src/sha3.h
@@ -1,35 +1,71 @@
 /**
- * sha3.h - SHA3 and Keccak hash function support header
+ * @file sha3.h
+ * @brief SHA3 hash function support.
+ * @details This file is based on Dr. Markku-Juhani O. Saarinen's
+ * "cooked up" compact and readable keccak implemetation...
+ * > <https://github.com/mjosaarinen/tiny_sha3>
+ * ... which was released under the MIT license (MIT).
+ * <br/><br/>
+ * Alterations to the transform function were based on
+ * Marko Kreen's optimized C implementation...
+ * > <https://github.com/markokr/spongeshaker>
+ * ... which was released under the ISC License (ISC).
  *
- * For more information, please refer to ./sha3.c
+ * @copyright 2014 Marko Kreen <markokr@gmail.com>
  *
- * Date: 22 April 2020
- * Revised: 26 October 2021
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
  *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @copyright 2015 Markku-Juhani O. Saarinen <mjos@iki.fi>
+ * @copyright 2020-2022 Adequate Systems, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * * The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef _CRYPTO_SHA3_H_
-#define _CRYPTO_SHA3_H_  /* include guard */
+/* include guard */
+#ifndef CRYPTO_SHA3_H
+#define CRYPTO_SHA3_H
 
 
-#include <stddef.h>  /* for size_t */
-#include "extint.h"
+#include "utildev.h"
 
-/* 64-bit rotate left definition */
-#ifndef ROTL64
-#define ROTL64(x, y)  ( ((x) << (y)) | ((x) >> (64 - (y))) )
-#endif
-
-/* SHA3 specific parameters */
-#define SHA3LEN224      28
-#define SHA3LEN256      32
-#define SHA3LEN384      48
-#define SHA3LEN512      64
-#define KECCAKLEN224    28
-#define KECCAKLEN256    32
-#define KECCAKLEN384    48
-#define KECCAKLEN512    64
-#define KECCAKFROUNDS   24
+#define SHA3LEN224      28 /**< SHA3 224-bit digest length, in bytes */
+#define SHA3LEN256      32 /**< SHA3 256-bit digest length, in bytes */
+#define SHA3LEN384      48 /**< SHA3 384-bit digest length, in bytes */
+#define SHA3LEN512      64 /**< SHA3 512-bit digest length, in bytes */
+#define KECCAKLEN224    28 /**< KECCAK 224-bit digest length, in bytes */
+#define KECCAKLEN256    32 /**< KECCAK 256-bit digest length, in bytes */
+#define KECCAKLEN384    48 /**< KECCAK 384-bit digest length, in bytes */
+#define KECCAKLEN512    64 /**< KECCAK 512-bit digest length, in bytes */
 
 /* SHA3 init and update routines mimmick keccak */
 #define keccak_init(c, len)          sha3_init(c, len)
@@ -37,30 +73,38 @@
 
 /* SHA3 context */
 typedef struct {
-   union {  /* state: */
-      word8 b[200];  /*  8-bit bytes  */
-      word32 d[50];  /*  32-bit words */
-      word64 q[25];  /*  64-bit words */
-   } st;
-   int pt, rsiz, outlen;  /* these don't overflow */
-} SHA3_CTX;
-typedef SHA3_CTX  KECCAK_CTX;
+   union {
+      uint8_t b[200];   /**< 8-bit input buffer */
+      uint64_t q[25];   /**< 64-bit input buffer */
+   } st;                /**< Input buffer union */
+   uint32_t outlen;     /**< Digest length, in bytes */
+   uint32_t rsiz;       /**< Rate size, in bytes */
+   uint32_t pt;         /**< Length of buffered input */
+   /**
+    * 256-bit alignment padding. Does nothing beyond ensuring
+    * a list of contexts that begin 256-bit aligned, remain
+    * similarly aligned for every item in said list.
+   */
+   uint32_t balign256[3];
+} SHA3_CTX;                    /**< SHA3 context */
+typedef SHA3_CTX  KECCAK_CTX;  /**< KECCAK context */
 
+/* C/C++ compatible function prototypes */
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Function prototypes for sha3.c */
-void sha3_init(SHA3_CTX *ctx, int outlen);
-void sha3_update(SHA3_CTX *ctx, const void *in, size_t inlen);
-void sha3_final(SHA3_CTX *ctx, void *out);
-void keccak_final(SHA3_CTX *ctx, void *out);
-void sha3(const void *in, size_t inlen, void *out, int outlen);
-void keccak(const void *in, size_t inlen, void *out, int outlen);
+HOST_DEVICE_FN void sha3_init(SHA3_CTX *ctx, int outlen);
+HOST_DEVICE_FN void sha3_update(SHA3_CTX *ctx, const void *in, size_t inlen);
+HOST_DEVICE_FN void sha3_final(SHA3_CTX *ctx, void *out);
+HOST_DEVICE_FN void keccak_final(SHA3_CTX *ctx, void *out);
+HOST_DEVICE_FN void sha3(const void *in, size_t inlen, void *out, int outlen);
+HOST_DEVICE_FN void keccak(const void *in, size_t inlen, void *out, int outlen);
 
+/* end extern "C" {} for C++ */
 #ifdef __cplusplus
 }
 #endif
 
-
-#endif  /* end _CRYPTO_SHA3_H_ */
+/* end include guard */
+#endif
