92c27103a66310a5cf5cdf31d94892a837551539
Chrisdigity <chrisdigity@gmail.com>
2022-04-13 12:22:39 +1000

    rebuild docs with new configuration

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['changelog_0',['Changelog',['../md__c_h_a_n_g_e_l_o_g.html',1,'']]],
-  ['creative_20commons_20legal_20code_1',['Creative Commons Legal Code',['../md__l_i_c_e_n_s_e.html',1,'']]],
-  ['crypto_2dc_2',['CRYPTO-C',['../index.html',1,'']]]
+  ['creative_20commons_20legal_20code_0',['Creative Commons Legal Code',['../md__l_i_c_e_n_s_e.html',1,'']]],
+  ['crypto_2dc_1',['CRYPTO-C',['../index.html',1,'']]]
 ];
