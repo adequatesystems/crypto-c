17a319fdb69d3f614351298e3e31a2320ea1f9f7
Chrisdigity <chrisdigity@gmail.com>
2025-01-24 18:22:06 +1000

    header to support (eventual) de-duplication of vectors across tests

---
+++ src/test/_vectors.h
@@ -0,0 +1,23 @@
+
+/* include guard */
+#ifndef CRYPTO_TEST_VECTORS_H
+#define CRYPTO_TEST_VECTORS_H
+
+
+#define NUMVECTORS   7
+#define MAXVECTORLEN 81
+
+/* Test vectors used in RFC 1321 */
+static char rfc_1321_vectors[NUMVECTORS][MAXVECTORLEN] = {
+   "",
+   "a",
+   "abc",
+   "message digest",
+   "abcdefghijklmnopqrstuvwxyz",
+   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+   "1234567890123456789012345678901234567890123456789012345678901234"
+   "5678901234567890"
+};
+
+/* end include guard */
+#endif
